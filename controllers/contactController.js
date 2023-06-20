f68cc9f682d09bd3bad2cde6c3fe7491e04f624b
blessedmadukoma <blessedmadukoma@gmail.com>
2023-06-20 17:21:31 +0100

    added async handler, mongoose

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,45 +1,47 @@
+const asyncHandler = require("express-async-handler");
+
 //@desc Get all contacts
 //@route GET /api/contacts
 //@access public
-const getContacts = (req, res) => {
+const getContacts = asyncHandler(async (req, res) => {
   res.status(200).json({ messsage: "Get all contacts" });
-};
+});
 
 //@desc Get contact
 //@route GET /api/contacts/:id
 //@access public
-const getContact = (req, res) => {
+const getContact = asyncHandler(async (req, res) => {
   res.status(200).json({ messsage: `Get contact for ${req.params.id}` });
-};
+});
 
 //@desc Create contact
 //@route POST /api/contacts
 //@access public
-const createContact = (req, res) => {
+const createContact = asyncHandler(async (req, res) => {
   console.log("The body is: ", req.body);
   const { name, email, phone } = req.body;
   if (!name || !email || !phone) {
     return res.status(400).json({ messsage: "Please enter all fields" });
   }
   res.status(201).json({ messsage: "Create Contaact" });
-};
+});
 
 //@desc Update contact
 //@route PUT /api/contacts/:id
 //@access public
-const updateContact = (req, res) => {
+const updateContact = asyncHandler(async (req, res) => {
   console.log("The body is: ", req.body);
   res.status(200).json({
     messsage: `Update contact for ${req.params.id}`,
   });
-};
+});
 
 //@desc Delete contact
 //@route DELETE /api/contacts/:id
 //@access public
-const deleteContact = (req, res) => {
+const deleteContact = asyncHandler(async (req, res) => {
   res.status(200).json({ messsage: `Delete contact for ${req.params.id}` });
-};
+});
 
 module.exports = {
   getContacts,
--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,11 +1,55 @@
+const { constants } = require("../constants");
+
 const errorHandler = (err, req, res, next) => {
   console.log("Error is: ", err);
   const statusCode = res.statusCode ? res.statusCode : 500;
 
-  res.status(statusCode).json({
-    message: err.message,
-    stackTrace: process.env.NODE_ENV === "production" ? null : err.stack,
-  });
+  switch (statusCode) {
+    case constants.VALIDATION_ERROR:
+      res.json({
+        title: "Validation Failed",
+        message: err.message,
+        stackTrace: err.stack,
+      });
+      break;
+    case constants.NOT_FOUND:
+      res.json({
+        title: "Not Found",
+        message: err.message,
+        stackTrace: err.stack,
+      });
+      break;
+    case constants.UNAUTHORIZED:
+      res.json({
+        title: "Unauthorized",
+        message: err.message,
+        stackTrace: err.stack,
+      });
+      break;
+    case constants.FORBIDDEN:
+      res.json({
+        title: "Forbidden",
+        message: err.message,
+        stackTrace: err.stack,
+      });
+      break;
+    case constants.INTERNAL_SERVER_ERROR:
+      res.json({
+        title: "Internal Server Error",
+        message: err.message,
+        stackTrace: err.stack,
+      });
+      break;
+
+    default:
+      console.log("No error, all good");
+      break;
+  }
+
+  // res.status(statusCode).json({
+  //   message: err.message,
+  //   stackTrace: process.env.NODE_ENV === "production" ? null : err.stack,
+  // });
 };
 
 module.exports = errorHandler;
