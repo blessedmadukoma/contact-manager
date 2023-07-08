28510ed6954661e29104c24a1318345e1adb17a9
blessedmadukoma <blessedmadukoma@gmail.com>
2023-07-09 00:19:03 +0100

    added jwt to login

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 
 //@desc Register a new user
@@ -45,7 +46,33 @@ const registerUser = asyncHandler(async (req, res) => {
 //@route POST /api/users/login
 //@access public
 const loginUser = asyncHandler(async (req, res) => {
-  res.json({ message: "Login this user" });
+  const { email, password } = req.body;
+  if (!email || !password) {
+    res.status(400);
+    throw new Error("All fields are mandatory!");
+  }
+
+  const user = await User.findOne({ email });
+
+  // check user and password
+  if (user && (await bcrypt.compare(password, user.password))) {
+    const accessToken = jwt.sign(
+      {
+        user: {
+          username: user.username,
+          email: user.email,
+          id: user._id,
+        },
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "5m",
+      }
+    );
+  } else {
+    res.status(401);
+    throw new Error("Invalid email or password!");
+  }
 });
 
 //@desc Current user
